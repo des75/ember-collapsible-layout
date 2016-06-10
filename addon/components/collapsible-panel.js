c1d29a5600cf3f3e0d168c9a4fac26bfbdcbae05
ED <des.binc@gmail.com>
2016-06-10 21:36:36 +0300

    basic layout changes, render css classes

--- addon/components/collapsible-panel.js
+++ addon/components/collapsible-panel.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNameBindings: ['isCollapsed:collapsible-panel-container--collapsed:collapsible-panel-container'],
+  classNameBindings: [
+    "isCollapsed:collapsible-panel-container--collapsed:collapsible-panel-container",
+    "alignmentNorth:collapsible-panel--north",
+    "alignmentEast:collapsible-panel--east",
+    "alignmentSouth:collapsible-panel--south",
+    "alignmentWest:collapsible-panel--west"
+  ],
+  
   actions: {
     collapsePanel(){
       this.set('isCollapsed', true);
@@ -10,6 +17,30 @@ export default Ember.Component.extend({
     expandPanel(){
       this.set('isCollapsed', false);
       this.sendAction('expandPanel');
+    }
+  },
+  
+  init(){
+    this._super(...arguments);
+
+    let config = this.get("config");
+
+    switch(config.region){
+    case "north":
+      this.set("alignmentNorth", true);
+      break;
+
+    case "east":
+      this.set("alignmentEast", true);
+      break;
+
+    case "south":
+      this.set("alignmentSouth", true);
+      break;
+
+    case "west":
+      this.set("alignmentWest", true);
+      break;
     }
   }
 });
